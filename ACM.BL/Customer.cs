560a1259a55ebe80c5acadbb4cb63122f51a5797
AllanKovac <allankfrasco@gmail.com>
2020-01-29 00:16:49 -0300

    First Commit
    Desenvolvendo a classe de customer e desenvolvendo o teste unitario da classe customer.

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public int IdCustomer { get; set; }
+        public string EmailAdress { get; set; }
+        public string FullName
+        {
+            get { 
+                return $"{LastName}, {FirstName}";
+            }
+        }
+        public string FirstName { get; set; }
+        private string _lastName;
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+    }
+}
